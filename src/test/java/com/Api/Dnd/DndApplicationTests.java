cf1a72c8f6fa6aa598559a4a31da40cae6d257b0
Zaasth <wellintonpaypal@gmail.com>
2024-08-20 22:02:28 -0300

    commit 1

---
+++ src/test/java/com/Api/Dnd/DndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Api.Dnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
